0d508dc256f817db1a9a9a7cc5b2b8fc7f695db2
vipulddalal <98317422+vipulddalal@users.noreply.github.com>
2023-12-29 08:08:37 +0530

    Update prime.test.js

--- prime.test.js
+++ prime.test.js
@@ -1,35 +1,31 @@
-const assert = require('chai').assert;
-const { isPrimeNumber } = require('./prime.js'); // Import your program's function
+// prime.test.js
 
-describe('isPrimeNumber', function () {
-    it('should return true for prime numbers', function () {
-        assert.isTrue(isPrimeNumber(2));
-        assert.isTrue(isPrimeNumber(3));
-        assert.isTrue(isPrimeNumber(5));
-        assert.isTrue(isPrimeNumber(7));
-        assert.isTrue(isPrimeNumber(11));
-        // Add more test cases for other prime numbers
-    });
+const isPrime = require('./prime');
 
-    it('should return false for non-prime numbers', function () {
-        assert.isFalse(isPrimeNumber(1));
-        assert.isFalse(isPrimeNumber(4));
-        assert.isFalse(isPrimeNumber(6));
-        assert.isFalse(isPrimeNumber(8));
-        assert.isFalse(isPrimeNumber(9));
-        // Add more test cases for other non-prime numbers
-    });
+describe('isPrime function', () => {
+  test('should return false for non-prime numbers', () => {
+    expect(isPrime(1)).toBe(false);
+    expect(isPrime(4)).toBe(false);
+    expect(isPrime(10)).toBe(false);
+  });
 
-    it('should handle negative numbers and zero appropriately', function () {
-        assert.isFalse(isPrimeNumber(0));
-        assert.isFalse(isPrimeNumber(-2));
-        // Add more test cases for negative numbers and zero
-    });
+  test('should return true for prime numbers', () => {
+    expect(isPrime(2)).toBe(true);
+    expect(isPrime(3)).toBe(true);
+    expect(isPrime(17)).toBe(true);
+  });
 
-    it('should handle invalid input (non-numeric input)', function () {
-        assert.isFalse(isPrimeNumber('abc'));
-        assert.isFalse(isPrimeNumber(null));
-        assert.isFalse(isPrimeNumber(undefined));
-        // Add more test cases for invalid input
-    });
+  test('should return false for negative numbers', () => {
+    expect(isPrime(-5)).toBe(false);
+    expect(isPrime(-10)).toBe(false);
+  });
+
+  test('should return false for non-integer numbers', () => {
+    expect(isPrime(5.5)).toBe(false);
+    expect(isPrime(10.2)).toBe(false);
+  });
+
+  test('should return false for strings', () => {
+    expect(isPrime('abc')).toBe(false);
+  });
 });
